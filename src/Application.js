1f74f0b18e568e9022e638fa7d218797a0395666
Collin Green <collin@collingreen.com>
2015-01-09 12:21:58 -0800

    feature (cache): add cached interstitials
    Add buttons and functionality for caching interstitials (added in 2.1.0
    of the chartboost plugin).

--- src/Application.js
+++ src/Application.js
@@ -26,6 +26,7 @@ exports = Class(GC.Application, function () {
 
     var buttonPadding = 20;
     var buttonWidth = (this.view.style.width - (buttonPadding * 2));
+    var buttonWidth2 = (this.view.style.width - (buttonPadding * 5)) / 2;
     var buttonHeight = 100;
     var buttonY = this.header.style.y + this.header.style.height + 100;
 
@@ -40,9 +41,32 @@ exports = Class(GC.Application, function () {
       onClick: bind(this, this.showInterstitial)
     });
 
-    var logViewY = this.showInterstitialButton.style.y +
-      this.showInterstitialButton.style.height +
-      150;
+    // cache interstitial
+    this.cacheInterstitialButton = new ButtonView({
+      superview: this.view,
+      x: buttonPadding,
+      y: this.showInterstitialButton.style.y + buttonHeight + 100,
+      width: buttonWidth,
+      height: buttonHeight,
+      title: "Cache Interstitial",
+      onClick: bind(this, this.cacheInterstitial)
+    });
+
+    // show interstitial if available
+    this.showInterstitialIfAvailableButton = new ButtonView({
+      superview: this.view,
+      x: buttonPadding,
+      y: this.cacheInterstitialButton.style.y + buttonHeight + 100,
+      width: buttonWidth,
+      height: buttonHeight,
+      title: "Show only if available",
+      onClick: bind(this, function () {
+        this.showInterstitial(true);
+      })
+    });
+
+    var logViewY = this.showInterstitialIfAvailableButton.style.y +
+      buttonHeight + 150;
     this.logView = new LogView({
       superview: this.view,
       x: 0,
@@ -50,11 +74,50 @@ exports = Class(GC.Application, function () {
       width: this.view.style.width,
       height: this.view.style.height - logViewY
     });
+
+    // listen for events
+    this._createEventListeners();
   };
 
-  this.showInterstitial = function () {
-    this.log("Showing chartboost interstitial");
-    chartboost.showInterstitial();
+  this._createEventListeners = function () {
+    chartboost.on('AdAvailable', bind(this, function () {
+      this.log("Ad cached and is available to view");
+    }));
+
+    chartboost.on('AdFailedToLoad', bind(this, function () {
+      this.log("Ad failed to load");
+    }));
+
+    chartboost.on('AdDisplayed', bind(this, function () {
+      this.log("Ad displayed to user");
+    }));
+
+    chartboost.on('AdDismissed', bind(this, function () {
+      this.log("Ad dismissed (clicked or closed)");
+    }));
+
+    chartboost.on('AdClicked', bind(this, function () {
+      this.log("Ad clicked!");
+    }));
+
+    chartboost.on('AdClosed', bind(this, function () {
+      this.log("Ad closed (not clicked)");
+    }));
+  };
+
+  this.showInterstitial = function (onlyIfAvailable) {
+    if (onlyIfAvailable) {
+      this.log("Showing chartboost interstitial (if available)");
+      chartboost.showInterstitialIfAvailable();
+    } else {
+      this.log("Showing chartboost interstitial");
+      chartboost.showInterstitial();
+    }
+  };
+
+  this.cacheInterstitial = function () {
+    this.log("Caching chartboost interstitial");
+    chartboost.cacheInterstitial();
   };
 
 
